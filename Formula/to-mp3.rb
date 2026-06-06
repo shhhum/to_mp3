a4ff2a028e9638bdd471b058df73249e1d05c9a9
shhhum <andrewjshum@gmail.com>
2026-06-07 02:52:01 +0800

    v0.1.1: pass -nostdin to ffmpeg so it stops eating the find stream

--- Formula/to-mp3.rb
+++ Formula/to-mp3.rb
@@ -1,9 +1,9 @@
 class ToMp3 < Formula
   desc "Recursively convert audio files to high-quality 320kbps MP3"
   homepage "https://github.com/shhhum/to_mp3"
-  url "https://github.com/shhhum/to_mp3/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://github.com/shhhum/to_mp3/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "REPLACE_AFTER_RELEASE"
-  version "0.1.0"
+  version "0.1.1"
 
   depends_on "ffmpeg"
 
